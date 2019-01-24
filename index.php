24415a762d461d3b2263f84c0a24f242bc3e3693
oicoru10 <46920229+oicoru10@users.noreply.github.com>
2019-01-24 14:12:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,7 +11,5 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-$array = array($result);
-
-var_dump($array);
+var_dump($result);
 ?>
