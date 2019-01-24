803f1cad037cc8ab1ec4e8a384d86b1610071bb6
oicoru10 <46920229+oicoru10@users.noreply.github.com>
2019-01-24 12:01:07 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,9 +1,3 @@
 <?php
-   $num = "+55888888888888";
-   $url = "https://api.telegram.org/bot"._TELEGRAM_BOT_TOKEN."/sendContact?chat_id=".$chat_id."&phone_number=".$num."&first_name=".$first_name;
-  $response = file_get_contents($url, false, NULL);
-  $jsondata = json_decode($response,true);
-  echo "<pre>";
-  print_r($jsondata);
-  echo "</pre><br>";
+  echo "สวัสดี LINE BOT";
 ?>
