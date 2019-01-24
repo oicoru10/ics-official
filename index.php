cc97f0e5336c11b49c7e5289163e51024f724dae
oicoru10 <46920229+oicoru10@users.noreply.github.com>
2019-01-24 14:39:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -11,23 +11,6 @@ curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
 $result = curl_exec($ch);
 curl_close($ch);
-var_dump($result);
-
-   $channelSecret = "ef410ca44d0502656720084f014c53fa";
-   $content = file_get_contents('php://input');
-   $arrayJson = json_decode($content, true);
-   $arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
-   $arrayHeader[] = "Authorization: Bearer {$accessToken}";
-   //รับข้อความจากผู้ใช้
-   $message = $arrayJson['events'][0]['message']['text'];
-   //รับ id ของผู้ใช้
-   $id = $arrayJson['events'][0]['source']['userId'];
-   
-   $chat_id = $arrayJson['events'][0]['message']['chat']['id'];
-   $first_name = $arrayJson['events'][0]['source']['displayName'];
-
-
-   var_dump($arrayHeader);
-   var_dump($arrayJson);
+print_r(explode('"', $result, 2));
+print_r(explode('"', $result, -1));
 ?>
