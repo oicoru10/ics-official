438f5d3bf33e9b70b961450ae729d0ac3a3b3e78
oicoru10 <46920229+oicoru10@users.noreply.github.com>
2019-01-24 11:59:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,9 @@
 <?php
-echo "Hello LINE BOT V1";
+   $num = "+55888888888888";
+   $url = "https://api.telegram.org/bot"._TELEGRAM_BOT_TOKEN."/sendContact?chat_id=".$chat_id."&phone_number=".$num."&first_name=".$first_name;
+  $response = file_get_contents($url, false, NULL);
+  $jsondata = json_decode($response,true);
+  echo "<pre>";
+  print_r($jsondata);
+  echo "</pre><br>";
 ?>
