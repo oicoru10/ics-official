7a9eebea0bb68145ec90ddb868c0cbbb8c237c0e
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-14 12:26:24 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,14 @@
 <?php
   echo "สวัสดี LINE BOT";
 
-$access_token = '/DJOA67LCUK/1Wt/YlQBhgRvCQh/bTA2H+6Oc9c2yt3N2YObVEgFXxlAw7/CCx5bY2R/8a25i+Nuir5h1c1FT9gg7GLKRjmtplSoPvF7lAiL9aFvsbNQV7eSlUBsTxWGhClOjBwfeAWgYnHov9/7aQdB04t89/1O/w1cDnyilFU=';
-$userId = 'U1433d8e7fabdefa79463b15e1924b4d0';
-$url = 'https://api.line.me/v2/bot/profile/'.$userId;
-$headers = array('Authorization: Bearer ' . $access_token);
+$access_token = '/djoa67lcuk/1wt/ylqbhgrvcqh/bta2h+6oc9c2yt3n2yobvegfxxlaw7/ccx5by2r/8a25i+nuir5h1c1ft9gg7glkrjmtplsopvf7lail9afvsbnqv7eslubstxwghclojbwfeawgynhov9/7aqdb04t89/1o/w1cdnyilfu=';
+$userid = 'u1433d8e7fabdefa79463b15e1924b4d0';
+$url = 'https://api.line.me/v2/bot/profile/'.$userid;
+$headers = array('authorization: bearer ' . $access_token);
 $ch = curl_init($url);
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+curl_setopt($ch, curlopt_returntransfer, true);
+curl_setopt($ch, curlopt_httpheader, $headers);
+curl_setopt($ch, curlopt_followlocation, 1);
 $result = curl_exec($ch);
 curl_close($ch);
 //echo "<BR>";
@@ -39,4 +39,16 @@ echo $json;
 $display = $array['displayName'];
 echo $display;
 
+$mystring = 'abc';
+$findme   = 'a';
+$pos = strpos($mystring, $findme);
+
+// Note our use of ===.  Simply == would not work as expected
+// because the position of 'a' was the 0th (first) character.
+if ($pos === false) {
+    echo "The string '$findme' was not found in the string '$mystring'";
+} else {
+    echo "The string '$findme' was found in the string '$mystring'";
+    echo " and exists at position $pos";
+}
 ?>
