37c244bb198324822c1cd3e7fd5aaf348f956735
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-14 18:08:36 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <?php
   echo "สวัสดี LINE BOT";
 echo "<BR>";
-$access_token = 'tpTjAZ5RC1rcUiqnVXDeVgdQJ0f+u0zf9MOYZQGlRlcEk64J6zH+QBpeZiJNjcfcY2R/8a25i+Nuir5h1c1FT9gg7GLKRjmtplSoPvF7lAgiTvdNMoscrt8aCG3aAD1irfEQjjDY2o+52Oq74j0MmQdB04t89/1O/w1cDnyilFU=';
-$userid = 'u1433d8e7fabdefa79463b15e1924b4d0';
-$url = 'https://api.line.me/v2/bot/profile/'.$userid;
-$headers = array('authorization: bearer ' . $access_token);
-$ch = curl_init($url);
-curl_setopt($ch, curlopt_returntransfer, true);
-curl_setopt($ch, curlopt_httpheader, $headers);
-curl_setopt($ch, curlopt_followlocation, 1);
-$result = curl_exec($ch);
-curl_close($ch);
+// $access_token = 'tpTjAZ5RC1rcUiqnVXDeVgdQJ0f+u0zf9MOYZQGlRlcEk64J6zH+QBpeZiJNjcfcY2R/8a25i+Nuir5h1c1FT9gg7GLKRjmtplSoPvF7lAgiTvdNMoscrt8aCG3aAD1irfEQjjDY2o+52Oq74j0MmQdB04t89/1O/w1cDnyilFU=';
+// $userid = 'u1433d8e7fabdefa79463b15e1924b4d0';
+// $url = 'https://api.line.me/v2/bot/profile/'.$userid;
+// $headers = array('authorization: bearer ' . $access_token);
+// $ch = curl_init($url);
+// curl_setopt($ch, curlopt_returntransfer, true);
+// curl_setopt($ch, curlopt_httpheader, $headers);
+// curl_setopt($ch, curlopt_followlocation, 1);
+// $result = curl_exec($ch);
+// curl_close($ch);
 //echo "<BR>";
 //$ar = explode('"', $result, -1);
 //var_dump($ar);
@@ -53,7 +53,7 @@ echo "<BR>";
 // $json = 'apiBlockTicketRequest:'.$data;
 // echo $json;
 
-echo $json;
+// echo $json;
 
 $str = '{
 					  "to": "U1433d8e7fabdefa79463b15e1924b4d0",
@@ -123,7 +123,7 @@ $str = '{
 	
 	$json = json_decode($str, true);
 	echo $json;
-	print_r($array);
+	print_r($json);
 // $display = $array['displayName'];
 // echo $display;
 
