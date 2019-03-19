a32313bc10b89c4c28f9b3155d082a32ac0a533c
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-19 15:55:18 +0700

    Update webhooks1.php

--- webhooks1.php
+++ webhooks1.php
@@ -444,14 +444,13 @@
 					}  ';
 				$json = json_decode($str, true);
 				pushMsg($arrayHeader,$json);
-				
-				
+								
 			case 'Date_form';
 				
-				$arrayPostData['to'] = $idTo;
-				$arrayPostData['messages'][0]['type'] = "text";
-				$arrayPostData['messages'][0]['text'] = "ตั้งแต่ วันที่ : " . $datepick;
-				pushMsg($arrayHeader,$arrayPostData);
+				// $arrayPostData['to'] = $idTo;
+				// $arrayPostData['messages'][0]['type'] = "text";
+				// $arrayPostData['messages'][0]['text'] = "ตั้งแต่ วันที่ : " . $datepick;
+				// pushMsg($arrayHeader,$arrayPostData);
 				// $str = ' { "to": "'. $idTo . '",
 					 // "messages": [
 						// {
