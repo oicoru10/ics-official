1bb6bd097e7b600f045329dbaac29f1990eef16a
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-19 10:59:01 +0700

    Update webhooks1.php

--- webhooks1.php
+++ webhooks1.php
@@ -144,7 +144,7 @@
 		  // // replyMsg($arrayHeader,$arrayPostData);
 		  // pushMsg($arrayHeader,$arrayPostData);
 		  
-		  $str = '{ "to": ' . $idTo . '
+		  $str = '{ "to": "U1433d8e7fabdefa79463b15e1924b4d0"
 					  "messages": [
 						{
 						  "type": "text",
@@ -191,7 +191,7 @@
      }
    elseif( strpos($message, 'เว็บ') !== false  OR strpos($message, 'Web') !== false )
    {
-	   $str1 = ' { "to": ' . $idTo . '
+	   $str1 = ' { "to": "U1433d8e7fabdefa79463b15e1924b4d0"
 					 "messages": [
 					  {
 					   "type": "flex",
