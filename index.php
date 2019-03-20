210cf65a1289bb97f81df69788f787303f5e7420
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-20 18:00:51 +0700

    Update index.php

--- index.php
+++ index.php
@@ -3,109 +3,45 @@
   echo "สวัสดี LINE BOT";
   echo "<BR>";
 
-
-// echo $json;
-// $servername = "remotemysql.com:3306";
-// $username = "OOd1POc2ro";
-// $password = "EtMy0i5bdp";
-// $dbname = "OOd1POc2ro";
-
-// // Create connection
-// $conn = new mysqli($servername, $username, $password, $dbname);
-// // Check connection
-// if ($conn->connect_error) {
-    // die("Connection failed: " . $conn->connect_error);
-// } 
-// // $display = $array['displayName'];
-// // echo $display;
-// // Create connection
-   // $idTo = 'U1433d8e7fabdefa79463b15e1924b4d0'.
-   // $sql = "SELECT Id_line,Name FROM Member";
-   // $result_sql = $conn->query($sql);
-   // if ($result_sql->num_rows > 0) {
-    // // output data of each row
-    // while($row = $result_sql->fetch_assoc()) {
-        // echo "id: " . $row["Id_line"]. " - Name: " . $row["Name"];
-    // }
-   // }
-   // else {
-    // echo "0 results";
-	// }
+	 // echo '<BR>';
+	// $client = new GuzzleHttp\Client();
+	// $res = $client->request('GET', 'GET http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet');
+	// $people = json_decode($res, true)['value'];
+	
+	// // $sap_wsdl = "http://thanagone.ku:p%40ssw0rd@vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet('00000001')";
+	// // $sap_client = new SoapClient($sap_wsdl);
+	// // $res = $sap_client->RFC_READ_TABLE();
+	   // if ( $people == null)
+	   // {
+		   // echo 'not connect';
+	   // }
+	   // else
+	   // {
+		   // echo $people;
+	   // }
 	   
-  // $conn->close();
-  
-  // echo date("Y-m-d");
-  
-	// $client = new GuzzleHttp\Client();
-	// $res = $client->request('GET', 'GET http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet("00000001")');
-	// $people = json_decode($res, true)['value'];
-	// // print_r($people[1]); // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
-	// echo $people; // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
-	/* connect to the OData service  */
-	
-	// $client = new GuzzleHttp\Client();
-	// $res = $client->request('GET', 'GET http://services.odata.org/TripPinRESTierService/People');
-	// $people = json_decode($res, true)['value'];
-	// echo $people; // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
-   
-	// $svc = new NorthwindEntities('http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV');
-     
-// /* get the list of Customers in the USA, for each customer get the list of Orders */
-    // $query = $svc->GetEmployeeListSet();
-    // $customerResponse = $query->Execute()
-						 // ->filter("EmployeeID = '00000001'");
-	
-	// echo $customerResponse;
+require_once "Connect.php";
+   //connect
+    $OData = new Z_WE_ASSCOCUSTOPENID_SRV_Entities('http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/') ;
+	$OData->Credential = new WindowsCredential('thanagone','p@ssw0rd');
+   try
+{
+    //Create a Customer php Object
+    $OpenId = OpenId::CreateOpenId( "","",$_POST["Name"],$_POST["CustomerName"],$_POST["Code"]);
+    //inserting Customers object context tracking system
+    $OData->AddObject('OpenIdSet', $OpenId);
+   //SaveChange insert the object into data service
+   $Odata->SaveChanges();
+}
+catch(ODataServiceException $exception)
+{
+    Echo $exception->getError();
+}
 
-// /* get only CustomerID and CustomerName */
-    // $query = $svc->Customers()
-                 // ->filter("Country eq 'USA'")
-                 // ->Select('CustomerID, CustomerName');
-    // $customerResponse = $query->Execute();
-
-// /* create a new customer */
-    // $customer = Customers::CreateCustomers('00000001', 'CHAN9');
-    // $proxy->AddToCustomers($customer); 
-
-// /* commit the change on the server */        
-    // $proxy->SaveChanges();
-	// $SOAP_AUTH= array( 'login' => 'thanagone.ku', 'password' => 'p@ssw0rd');
-	// #SpecifyWSDL
-	// $WSDL="http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet";
-	// #CreateClient Object, download and parse WSDL
-	// $client= new SoapClient($WSDL,$SOAP_AUTH);
-	// $HEAD_DATA= new stdClass();
-	// $HEAD_DATA->EmployeeID = '00000001';
-	// #Setup input parameters (SAP Likes to Capitalise the parameter names)
-	// $params= array('HEADDATA' => $HEAD_DATA );
-	// #Call Operation (Function). Catch and display any errors
-	// try {
-	   // $result = $client->StandardMaterialSaveData($params);
-	// }catch(SoapFault $exception) {
-	   // print "***Caught Exception***\n";
-	   // print_r($exception);
-	   // print "***END Exception***\n";
-	   // die();
-	// }
-	// #Out the results
-	// print_r($result);
-	// error:SOAP-ERROR: Encoding: object has no 'HeadData' property
-	 echo '<BR>';
-	$client = new GuzzleHttp\Client();
-	$res = $client->request('GET', 'GET http://thanagone.ku:p%40ssw0rd@vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet');
-	$people = json_decode($res, true)['value'];
-	
-	$sap_wsdl = "http://thanagone.ku:p%40ssw0rd@vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet('00000001')";
-	$sap_client = new SoapClient($sap_wsdl);
-	$res = $sap_client->RFC_READ_TABLE();
-	   if ( $res == null)
-	   {
-		   echo 'not connect';
-	   }
-	   else
-	   {
-		   echo $res;
-	   }
+foreach ($result as $resultx ){
+            foreach ($resultx as $v){
+                echo ("test:" .  $v);
+            }}
 	// foreach ($people as $value)
    // {
       // $empp = $value->EmployeeID;
