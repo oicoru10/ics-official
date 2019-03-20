4120949b09766a4d0423647097955e14f894b859
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-20 15:14:05 +0700

    Update index.php

--- index.php
+++ index.php
@@ -85,20 +85,20 @@ echo "<BR>";
   
   // echo date("Y-m-d");
   
-	$client = new GuzzleHttp\Client();
-	$res = $client->request('GET', 'GET http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet("00000001")');
-	$people = json_decode($res, true)['value'];
-	// print_r($people[1]); // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
-	echo $people; // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
+	// $client = new GuzzleHttp\Client();
+	// $res = $client->request('GET', 'GET http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet("00000001")');
+	// $people = json_decode($res, true)['value'];
+	// // print_r($people[1]); // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
+	// echo $people; // ["UserName" => "russellwhyte", "FirstName" => "Russell" ...]
 	/* connect to the OData service  */
-   // $svc = new NorthwindEntities('http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV');
+   $svc = new NorthwindEntities('http://vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV');
      
-// /* get the list of Customers in the USA, for each customer get the list of Orders */
-    // $query = $svc->GetEmployeeListSet();
-    // $customerResponse = $query->Execute()
-						 // ->filter("EmployeeID = '00000001'");
+/* get the list of Customers in the USA, for each customer get the list of Orders */
+    $query = $svc->GetEmployeeListSet();
+    $customerResponse = $query->Execute()
+						 ->filter("EmployeeID = '00000001'");
 	
-	// echo $customerResponse[1];
+	echo $customerResponse;
 
 // /* get only CustomerID and CustomerName */
     // $query = $svc->Customers()
