51d7a0576956bed805a8c9b6ef0df42f78ef83c9
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-21 11:40:42 +0700

    Update index.php

--- index.php
+++ index.php
@@ -64,7 +64,8 @@
    $content = file_get_contents('php://input');
    $arrayJson = json_decode($content, true);
    $arrayHeader = array();
-   $arrayHeader[] = "Content-Type: application/json";
+   // $arrayHeader[] = "Content-Type: application/json";
+   $arrayHeader[] = "Content-Type:  text/xml";
    // $arrayHeader[] = "Authorization: Bearer {$accessToken}";
 	
    $url = 'http://thanagone.ku:p%40ssw0rd@vms4ics.ics-th.com:8000/sap/opu/odata/sap/ZPROFILE_SRV/GetEmployeeListSet';
