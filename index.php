7eca2e6b004eb7b5baa911086e817d13e6e61ccc
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-21 13:45:26 +0700

    Update index.php

--- index.php
+++ index.php
@@ -101,6 +101,27 @@
 			$ns = $item->content->children('http://schemas.microsoft.com/ado/2007/08/dataservices/metadata'); 
 			$nsd = $ns->properties->children("http://schemas.microsoft.com/ado/2007/08/dataservices");
 			// print_r($ns->properties); 
+			foreach ($nsd as $va) {
+				
+				echo "EmployeeID : " . $va->EmployeeID;
+				echo "<BR>";
+				echo "Firstname : " . $va->Firstname;
+				echo "<BR>";
+				echo "Lastname : " . $va->Lastname;
+				echo "<BR>";
+				echo "Nickname : " . $va->Nickname;
+				echo "<BR>";
+				echo "Tel : " . $va->Tel;
+				echo "<BR>";
+				echo "Email : " . $va->Email;
+				echo "<BR>";
+				echo "Birthdate : " . $va->Birthdate;
+				echo "<BR>";
+				echo "Position : " . $va->Position;
+				echo "<BR>";
+				echo "Line : " . $va->Line;
+				echo "<BR>";
+			}
 			print_r($nsd); 
 		}
 		// echo "<BR>";
