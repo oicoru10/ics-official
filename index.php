b94bd54f27f3c419091c6f97cbd4a2a37d17d3c6
Thanagone <46920229+oicoru10@users.noreply.github.com>
2019-03-21 11:51:09 +0700

    Update index.php

--- index.php
+++ index.php
@@ -77,19 +77,20 @@
    curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
    $result = curl_exec($ch);
    curl_close($ch);
-   $ob= simplexml_load_string($result);
+   $ob = simplexml_load_string($result);
    $json_string = '[' . $result . ']';
    $Profile = json_decode($json_string);
-   if ( $ob == null)
-   {
+   // if ( $ob == null)
+   // {
 	   echo 'not connect';
 	   echo "<BR>";
 	   echo $result;
-   }
-   else
-   {
 	   echo $ob;
-   }
+   // }
+   // else
+   // {
+	   // echo $ob;
+   // }
    
 ?>
 
